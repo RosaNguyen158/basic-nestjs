d355507e8ed56799c720756b32305a9fbb8163d3
Rosa <rosa.nguyen.goldenowl@gmail.com>
2022-09-07 22:55:20 +0700

    test Husky

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -12,7 +12,7 @@ export class AuthController {
   }
 
   @Post('/signin')
-  signIn(
+  signI(
     @Body() authCredentialsDto: AuthCredentialsDto,
   ): Promise<{ accessToken: string }> {
     return this.authService.signIn(authCredentialsDto);
